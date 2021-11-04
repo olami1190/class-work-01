8d29da6095fc03fef26b5c89c5edce1f940213fa
oladimeji idowu <oladimeji.idowu001@gmail.com>
2021-11-04 00:05:23 -0400

    build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/project.html templates/bottom.html > docs/project.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
